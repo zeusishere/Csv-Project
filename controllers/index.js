3bdf69fef9c80040ba6161dd505c8986be2c3b45
Shubham <shubham1251997@gmail.com>
2021-10-03 17:19:17 +0530

    added pagination

--- controllers/index.js
+++ controllers/index.js
@@ -19,30 +19,98 @@ module.exports.uploadFile = (req, res) => {
         return res.redirect("back");
     })
 }
+// module.exports.viewFile = async (req, res) => {
+//     let headers = [];
+//     let dataArray = [];
+//     // or rather use id here
+//     let fileId = req.query.fileId;
+//     console.log(fileId) ;
+//     let fileObj = await File.findById(fileId);
+//     await fs.createReadStream(path.join(__dirname, "..", fileObj.filePath))
+//         .pipe(csv())
+//         .on('headers', (header_arr) => {
+
+//             headers = header_arr;
+//         })
+//         .on('data', (row) => {
+//             // console.log(row) ;
+//             dataArray.push(row);
+//         })
+//         .on('end', () => {
+//             console.log("csv file successfully processed");
+//             // console.log(headers);
+//             // console.log(dataArray)
+//             res.render("viewFile", {
+//                 headers: headers
+//                 , dataArray: dataArray
+//             })
+//         });
+
+// }
 module.exports.viewFile = async (req, res) => {
     let headers = [];
     let dataArray = [];
     // or rather use id here
     let fileId = req.query.fileId;
+    let pageNo = parseInt(req.query.page) ;
+    // skip lines as
+    let skip= pageNo*10 ;let limit = 10;
+    if (pageNo==0)
+    {
+        skip=0 ;
+    }
+    let paginationObj = {} ;
     console.log(fileId) ;
     let fileObj = await File.findById(fileId);
-    await fs.createReadStream(path.join(__dirname, "..", fileObj.filePath))
-        .pipe(csv())
-        .on('headers', (header_arr) => {
-
-            headers = header_arr;
-        })
+    var headerStream =await fs.createReadStream(path.join(__dirname, "..", fileObj.filePath))
+    .pipe(csv())
+    .on('headers', (header_arr) => {
+        headers = header_arr;
+        console.log( header_arr);
+        headerStream.destroy();
+    }) ;
+    var readStream = await fs.createReadStream(path.join(__dirname, "..", fileObj.filePath))
+        .pipe(csv({skipLines :skip}))
+        
         .on('data', (row) => {
             // console.log(row) ;
             dataArray.push(row);
+            limit-- ;
+            if (limit==0)
+            {
+                readStream.destroy();
+                console.log("csv file successfully processed");
+            // console.log(headers);
+            // console.log(dataArray)
+            paginationObj["prevClass"]=((pageNo <= 0)?"disabled":"") ;
+            paginationObj["nextClass"]=("") ;
+
+            res.render("viewFile", {
+                headers: headers
+                , dataArray: dataArray,
+                pageNo :pageNo,
+                fileId:fileId,
+                paginationObj :paginationObj ,
+                homeLink:true 
+                
+            })
+            }
+
+           
         })
         .on('end', () => {
             console.log("csv file successfully processed");
             // console.log(headers);
-            // console.log(dataArray)
+            // console.log(dataArray);
+            paginationObj["prevClass"]=((pageNo <= 0)?"disabled":"") ;
+            paginationObj["nextClass"]=("disabled") ;
             res.render("viewFile", {
                 headers: headers
-                , dataArray: dataArray
+                , dataArray: dataArray,
+                pageNo :pageNo,
+                fileId:fileId,
+                paginationObj :paginationObj,
+                homeLink:true 
             })
         });
 
