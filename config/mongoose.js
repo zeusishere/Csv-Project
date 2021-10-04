09249bf0773bc345d1ca121266ba24875bd920f9
Shubham <shubham1251997@gmail.com>
2021-10-04 16:05:01 +0530

    final draft

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
 
 const mongoose = require("mongoose");
-// mongoose.connect('mongodb://localhost:27017/csvDB',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   }
-// );
-mongoose.connect('mongodb+srv://shubham:2424554@cluster0.e6nsv.mongodb.net/CvsDb?retryWrites=true&w=majority')
+mongoose.connect('mongodb://localhost:27017/csvDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+// mongoose.connect('mongodb+srv://shubham:2424554@cluster0.e6nsv.mongodb.net/CvsDb?retryWrites=true&w=majority')
 
 const db = mongoose.connection;
 db.on("error",  console.error.bind(console,"connection error :cannot connect to the db"));
